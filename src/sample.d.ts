8d8393c1b13d2fe145c683a93071b5a44ac8bb6b
sota <iwaso5323@gmail.com>
2025-08-14 14:31:30 +0900

    不要なサンプルファイルを削除

--- src/sample.d.ts
+++
@@ -1,9 +0,0 @@
-export type TState = {
-    name: string;
-    capital: string;
-};
-export interface IState {
-    name: string;
-    capital: string;
-}
-//# sourceMappingURL=sample.d.ts.map
--- src/sample.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// TypeScriptのサンプルコード
-Object.defineProperty(exports, "__esModule", { value: true });
-const wyoming = {
-    name: 'Wyoming',
-    capital: 'Cheyenne',
-};
-console.log(wyoming);
-//# sourceMappingURL=sample.js.map
